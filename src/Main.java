d1e7910f722f91313c7c8253ea83dc0223ec48c6
Mike4977 <weilliss@mail.ru>
2023-06-09 15:21:54 +0600

    ??

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,21 @@ public class Main {
         System.out.println(n);
 
         int count = 0;
+        int[] m1 = new int[Short.MAX_VALUE];
         for (int j = i; j <= Short.MAX_VALUE; j++) {
 
-            if (j % n == 0) System.out.println(j);
+            if (j % n == 0) m1[count] = j;
+            if (m1[count] != 0) System.out.print(m1[count] + "; ");
             count++;
         }
-        for (int k = Short.MIN_VALUE; k <= i ; k++) {
-            if (i % n != 0) System.out.println(k);
+        System.out.println(" ");
+        System.out.println("++++++++++++++++++");
+
+        count = 0;
+        int[] m2 = new int[Math.abs(Short.MIN_VALUE) + i];
+        for (int k = Short.MIN_VALUE ; k < i ; k++) {
+            if (i % n != 0) m2[count] = k;
+            if (m2[count] != 0) System.out.print(m2[count] + "; ");
             count++;
         }
     }
